00142f720a8ea1051a19b6fee33a3af5e692f2a4
misha <milisov4@gmal.com>
2023-04-05 10:40:57 +0300

    improved categories controller and tests

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,36 @@
 class CategoriesController < ApplicationController
+  before_action :check_request
+
   def index
     @movies = Movie.where(category: categories)
   end
 
   private
 
-  def categories
-    return Movie.defined_enums['category'].keys unless params[:categories]
+  def check_request
+    redirect_to root_path unless turbo_frame_request?
+  end
 
-    params[:categories].split('-')
+  def categories
+    return valid_categoties unless params[:categories]
+
+    common_categories.empty? ? show_alert : common_categories
+  end
+
+  def show_alert
+    flash.alert = 'Invalid category'
+    redirect_to root_path
+  end
+
+  def valid_categoties
+    @_valid_categoties ||= Movie.defined_enums['category'].keys
+  end
+
+  def common_categories
+    @_common_categories ||= valid_categoties.intersection(params_categories)
+  end
+
+  def params_categories
+    @_params_categories ||= params[:categories].split('-')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :movies, except: :index, constraints: { id: /\d.+/ } do
-    get 'categories', to: 'categories#index', as: :all_categories, on: :collection
-    get 'categories/:categories', to: 'categories#index', as: :categories, on: :collection
+  resources :movies, except: :index do
+    get 'categories/(:categories)', to: 'categories#index', as: :categories, on: :collection
     resources :ratings, only: %i[create update destroy]
   end
 
