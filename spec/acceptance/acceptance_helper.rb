273925a2b3d9dd868030004c80535e6fc485acbd
misha <milisov4@gmal.com>
2023-04-05 14:55:53 +0300

    configured capybara

---
+++ spec/acceptance/acceptance_helper.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.configure do |config|
+  Capybara.register_driver :chrome do |app|
+    Capybara::Selenium::Driver.new app, browser: :chrome,
+      options: Selenium::WebDriver::Chrome::Options.new(args: %w[headless disable-gpu])
+  end
+
+  Capybara.javascript_driver = :chrome
+
+  config.use_transactional_fixtures = false
+
+  config.include Warden::Test::Helpers, type: :feature
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    Warden.test_mode!
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+    Warden.test_reset!
+  end
+end
