f98bce7356754ada3505239d00b8a047550a59d9
misha <milisov4@gmal.com>
2023-04-05 22:25:18 +0300

    fixed rating count job with tests

--- app/jobs/rating_count_job.rb
+++ app/jobs/rating_count_job.rb
@@ -2,7 +2,8 @@ class RatingCountJob < ApplicationJob
   def perform(movie_id)
     movie = Movie.find(movie_id)
     ratings = Rating.where(movie_id:).pluck(:points)
-    new_rating = (ratings.sum / ratings.count).round(2)
+    count = ratings.count
+    new_rating = count.zero? ? 0 : (ratings.sum / count).round(2)
     movie.update!(rating: new_rating)
   end
 end
--- spec/jobs/rating_count_job_spec.rb
+++ spec/jobs/rating_count_job_spec.rb
@@ -4,6 +4,16 @@ RSpec.describe RatingCountJob do
   subject(:job) { described_class.perform_later(movie.id) }
   let!(:movie) { create(:movie, :with_ratings, rating: 5) }
 
+  context 'when count is 0' do
+    let!(:movie) { create(:movie) }
+
+    it 'sets rating to 0' do
+      perform_enqueued_jobs { job }
+      movie.reload
+      expect(movie.rating).to eq(0)
+    end
+  end
+
   context 'when created' do
     before { create(:rating, points: 9, rated_movie: movie) }
 
