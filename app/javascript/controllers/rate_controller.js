bbe85183da1333b1c27f9a0f15fa41aa9fa0f03b
misha <milisov4@gmal.com>
2023-04-06 11:27:31 +0300

    created and improved pop-up rate form

---
+++ app/javascript/controllers/rate_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [ 'rateStar', 'rateBtn' ]
+
+  chooseRate({params}) {
+    this.rateStarTargets.forEach(element => {
+      if (element.dataset.number <= params.number) {
+        element.classList.remove("movie-text", "bi-star");
+        element.classList.add("star-color", "bi-star-fill");
+      } else {
+        element.classList.remove("star-color", "bi-star-fill");
+        element.classList.add("movie-text", "bi-star");
+      }
+    });
+    this.rateBtnTarget.classList.remove('disabled')
+  }
+}
