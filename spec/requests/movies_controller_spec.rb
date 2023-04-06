342fa99c032f8025d4b27ee32805dff30e7d8c4e
misha <milisov4@gmal.com>
2023-04-06 20:16:20 +0300

    updated rating

--- spec/requests/movies_controller_spec.rb
+++ spec/requests/movies_controller_spec.rb
@@ -15,6 +15,54 @@ RSpec.describe MoviesController do
     subject(:show_request) { get "/movies/#{movie.id}" }
     let(:movie) { create(:movie) }
 
+    describe 'finding rating' do
+      let(:user) { create(:user) }
+
+      context 'with rating' do
+        let(:rating) { create(:rating, rated_movie: movie, rated_user: user) }
+
+        context 'with non-authenticated user' do
+          it 'does not find rating' do
+            rating
+            show_request
+            expect(assigns(:rating)).to eq(nil)
+          end
+        end
+
+        context 'with non-authenticated user' do
+          before { sign_in user }
+
+          it 'does not find rating' do
+            rating
+            show_request
+            expect(assigns(:rating)).to eq(rating)
+          end
+        end
+      end
+
+      context 'without rating' do
+        let(:rating) { nil }
+
+        context 'with non-authenticated user' do
+          it 'does not find rating' do
+            rating
+            show_request
+            expect(assigns(:rating)).to eq(nil)
+          end
+        end
+
+        context 'with non-authenticated user' do
+          before { sign_in user }
+
+          it 'does not find rating' do
+            rating
+            show_request
+            expect(assigns(:rating)).to eq(nil)
+          end
+        end
+      end
+    end
+
     include_examples 'finds movie'
     include_examples 'positive GET responses', :show
   end
