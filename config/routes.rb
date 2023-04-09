57eb69ec9f0390491664bec75f3bb03a73e03fae
misha <milisov4@gmal.com>
2023-04-09 12:24:10 +0300

    updated movies controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :movies, except: :index do
+  resources :movies, only: %i[index show] do
     get 'categories/(:categories)', to: 'categories#index', as: :categories, on: :collection
     resources :ratings, only: %i[new edit create update destroy]
   end
