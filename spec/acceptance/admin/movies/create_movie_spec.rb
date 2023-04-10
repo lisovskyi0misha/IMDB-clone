35fadc67a022c564aa26275a73c334ff9a0f8f4a
misha <milisov4@gmal.com>
2023-04-10 10:37:17 +0300

    admin create movie feature with tests

---
+++ spec/acceptance/admin/movies/create_movie_spec.rb
@@ -0,0 +1,30 @@
+require_relative '../../acceptance_helper'
+
+feature 'create movie', %(
+  Only admins can create movie
+) do
+  scenario 'Non-authenticated user tries to create a movie' do
+    visit new_admin_movie_path
+    expect(page).to have_content('You need to sign in or sign up before continuing')
+  end
+
+  scenario 'Authenticated regular user tries to create a movie' do
+    sign_in(create(:user))
+    visit new_admin_movie_path
+    expect(page).to have_content('You don\'t have rights to access this page')
+  end
+
+  scenario 'Admin tries to creatre a movie' do
+    sign_in(create(:user, :admin))
+    visit new_admin_movie_path
+    fill_in 'Title', with: 'Title 1'
+    fill_in 'Text', with: 'Some movie description text'
+    attach_file 'Image', "#{Rails.root}/spec/fixtures/test_image.png"
+    attach_file 'Trailer', "#{Rails.root}/spec/fixtures/test_video.mp4"
+    click_on 'Create'
+    expect(page).to have_content('Movie was successfully created')
+    visit admin_movie_path(id: Movie.first.id)
+    expect(page).to have_css("img[src='#{url_for(Movie.first.image.variant(resize_to_limit: [600, 600]))}']")
+    expect(page).to have_css("video[src*='#{rails_blob_path(Movie.first.trailer)}']")
+  end
+end
