74ce389cfc4d0eb95e94b4edd0c9596c1b96d031
misha <milisov4@gmal.com>
2023-04-11 15:36:40 +0300

    added sendgrid api key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  ActionMailer::Base.smtp_settings = {
+    user_name: 'apikey',
+    password: Rails.application.credentials.dig(:sendgrid_api_key),
+    domain: ENV['DOMAIN'],
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 end
