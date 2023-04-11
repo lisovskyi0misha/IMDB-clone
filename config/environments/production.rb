b8cbe145c69dcedfd38221b5d9041560f8165346
misha <milisov4@gmal.com>
2023-04-11 15:46:19 +0300

    mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "IMDB_clone_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliviries = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
